a0e73510e8cdab3ae414ec896a7a5c945f9b256e
jcunanan05 <j_cunanan05@yahoo.com>
2018-09-13 13:34:42 -0400

    Arrange Calculator Buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,23 +2,26 @@ import React from 'react';
 import CalculatorKey from './CalculatorKey';
 import calculatorKeyList from '../calculatorKeyList';
 
+/* eslint-disable react/no-array-index-key */
+function renderKeys() {
+  return calculatorKeyList.map((calculatorKey, i) => (
+    <CalculatorKey key={i} character={calculatorKey} />
+  ));
+}
+
 const Calculator = () => (
   <section className="calculator">
     <div className="calculator__display">
       <section className="calculator__formula">
-        <h2 className="title is-5">One + One</h2>
+        <h2 className="title is-5">1 + 1</h2>
       </section>
 
       <section className="calculator__result">
-        <h2 className="title">9</h2>
+        <h2 className="title is-1">9</h2>
       </section>
     </div>
 
-    <div className="calculator__body">
-      {calculatorKeyList.map((calculatorKey) => (
-        <CalculatorKey key={calculatorKey} character={calculatorKey} />
-      ))}
-    </div>
+    <div className="calculator__body">{renderKeys()}</div>
   </section>
 );
 
