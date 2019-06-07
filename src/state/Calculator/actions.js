0b35bd0aa73ce42d842393ee513ae6991e3e6e6a
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-06 21:02:49 -0400

    Add register operations actions

--- src/state/Calculator/actions.js
+++ src/state/Calculator/actions.js
@@ -4,35 +4,63 @@ import {
   CLEAR_VALUE,
   REVERSE_SIGN,
   REGISTER_ADD_OPERATION,
+  REGISTER_SUBTRACT_OPERATION,
+  REGISTER_MULTIPLY_OPERATION,
+  REGISTER_DIVIDE_OPERATION,
   SAVE_TO_PREVIOUS_VALUE,
   EQUAL_OPERATION,
 } from './constants';
 
-export const updateValue = value => ({
+const updateValue = value => ({
   type: UPDATE_VALUE,
   value,
 });
 
-export const putDecimal = () => ({
+const putDecimal = () => ({
   type: PUT_DECIMAL,
 });
 
-export const clearValue = () => ({
+const clearValue = () => ({
   type: CLEAR_VALUE,
 });
 
-export const reverseSign = () => ({
+const reverseSign = () => ({
   type: REVERSE_SIGN,
 });
 
-export const registerAddOperation = () => ({
+const registerAddOperation = () => ({
   type: REGISTER_ADD_OPERATION,
 });
 
-export const saveToPreviousValue = () => ({
+const registerSubtractOperation = () => ({
+  type: REGISTER_SUBTRACT_OPERATION,
+});
+
+const registerMultiplyOperation = () => ({
+  type: REGISTER_MULTIPLY_OPERATION,
+});
+
+const regsiterDivideOperation = () => ({
+  type: REGISTER_DIVIDE_OPERATION,
+});
+
+const saveToPreviousValue = () => ({
   type: SAVE_TO_PREVIOUS_VALUE,
 });
 
-export const equalOperation = () => ({
+const equalOperation = () => ({
   type: EQUAL_OPERATION,
 });
+
+export {
+  updateValue,
+  putDecimal,
+  clearValue,
+  reverseSign,
+  registerAddOperation,
+  registerSubtractOperation,
+  registerMultiplyOperation,
+  regsiterDivideOperation,
+  saveToPreviousValue,
+  equalOperation,
+};
