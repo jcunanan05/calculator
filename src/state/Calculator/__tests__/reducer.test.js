66ec469d332fab068f6d09181fb156f3083365f9
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-06 20:56:59 -0400

    Group the tests

--- src/state/Calculator/__tests__/reducer.test.js
+++ src/state/Calculator/__tests__/reducer.test.js
@@ -32,60 +32,70 @@ test('clearValue must have 0 in display and value', () => {
   });
 });
 
-test('putDecimal input 0 , 1 must output .1', () => {
-  store.dispatch(putDecimal());
-  store.dispatch(updateValue(1));
-  expect(store.getState()).toMatchObject({
-    display: '.1',
-    value: 0.1,
+describe('putting decimal', () => {
+  test('putDecimal input 0 , 1 must output .1', () => {
+    store.dispatch(putDecimal());
+    store.dispatch(updateValue(1));
+    expect(store.getState()).toMatchObject({
+      display: '.1',
+      value: 0.1,
+    });
+  });
+
+  test('putDecimal does not double decimal', () => {
+    store.dispatch(putDecimal());
+    store.dispatch(updateValue(1));
+    store.dispatch(putDecimal());
+    expect(store.getState()).toMatchObject({
+      display: '.1',
+      value: 0.1,
+    });
   });
 });
 
-test('putDecimal does not double decimal', () => {
-  store.dispatch(putDecimal());
-  store.dispatch(updateValue(1));
-  store.dispatch(putDecimal());
-  expect(store.getState()).toMatchObject({
-    display: '.1',
-    value: 0.1,
+describe('reverse Sign', () => {
+  test('reverseSign input 0 must output 0', () => {
+    store.dispatch(reverseSign());
+    expect(store.getState()).toMatchObject({
+      display: '0',
+      value: 0,
+    });
+  });
+  test('reverseSign input 1000 must output -1000', () => {
+    store.dispatch(updateValue(1000));
+    store.dispatch(reverseSign());
+    expect(store.getState()).toMatchObject({
+      display: '-1000',
+      value: -1000,
+    });
+  });
+  test('reverseSign input -1000 must output 1000', () => {
+    store.dispatch(updateValue(-1000));
+    store.dispatch(reverseSign());
+    expect(store.getState()).toMatchObject({
+      display: '1000',
+      value: 1000,
+    });
   });
 });
 
-test('reverseSign input 0 must output 0', () => {
-  store.dispatch(reverseSign());
-  expect(store.getState()).toMatchObject({
-    display: '0',
-    value: 0,
+describe('add operation', () => {
+  test('registers', () => {
+    store.dispatch(registerAddOperation());
+    expect(store.getState()).toMatchObject({
+      operation: signs.PLUS,
+    });
   });
-});
-
-test('reverseSign input 1000 must output -1000', () => {
-  store.dispatch(updateValue(1000));
-  store.dispatch(reverseSign());
-  expect(store.getState()).toMatchObject({
-    display: '-1000',
-    value: -1000,
-  });
-});
-
-test('reverseSign input -1000 must output 1000', () => {
-  store.dispatch(updateValue(-1000));
-  store.dispatch(reverseSign());
-  expect(store.getState()).toMatchObject({
-    display: '1000',
-    value: 1000,
-  });
-});
-
-test('perform add operation works', () => {
-  store.dispatch(updateValue(100));
-  store.dispatch(registerAddOperation());
-  store.dispatch(saveToPreviousValue());
-  store.dispatch(clearValue());
-  store.dispatch(updateValue(20));
-  store.dispatch(equalOperation());
-  expect(store.getState()).toMatchObject({
-    display: '120',
-    value: 120,
+  test('perform add operation works', () => {
+    store.dispatch(updateValue(100));
+    store.dispatch(registerAddOperation());
+    store.dispatch(saveToPreviousValue());
+    store.dispatch(clearValue());
+    store.dispatch(updateValue(20));
+    store.dispatch(equalOperation());
+    expect(store.getState()).toMatchObject({
+      display: '120',
+      value: 120,
+    });
   });
 });
