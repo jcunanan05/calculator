9e7b8b2b9c4e24ce8ee8a689bd6f6430f39d8a89
jcunanan05 <j_cunanan05@yahoo.com>
2019-07-11 18:05:04 -0400

    Rewrite

--- src/state/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import calculator from './Calculator/reducer';
-
-export default combineReducers({
-  calculator,
-});
--- src/views/components/Calculator/Display.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './Display.css';
-
-export default function Display({ value }) {
-  return <section className="display">{value}</section>;
-}
