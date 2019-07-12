559ef3b155e60773e24cdf87fb1179f128bcb184
jcunanan05 <j_cunanan05@yahoo.com>
2019-07-11 20:12:23 -0400

    Add frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,223 @@
 import React from 'react';
-import logo from './logo.svg';
+// import PointTarget from 'react-point';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AutoScalingText extends React.Component {
+  state = {
+    scale: 1,
+  };
+
+  componentDidUpdate() {
+    const { scale } = this.state;
+
+    const node = this.node;
+    const parentNode = node.parentNode;
+
+    const availableWidth = parentNode.offsetWidth;
+    const actualWidth = node.offsetWidth;
+    const actualScale = availableWidth / actualWidth;
+
+    if (scale === actualScale) return;
+
+    if (actualScale < 1) {
+      this.setState({ scale: actualScale });
+    } else if (scale < 1) {
+      this.setState({ scale: 1 });
+    }
+  }
+
+  render() {
+    const { scale } = this.state;
+
+    return (
+      <div
+        className="auto-scaling-text"
+        style={{ transform: `scale(${scale},${scale})` }}
+        ref={node => (this.node = node)}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
-export default App;
+class CalculatorDisplay extends React.Component {
+  render() {
+    const { value, ...props } = this.props;
+
+    const language = navigator.language || 'en-US';
+    let formattedValue = parseFloat(value).toLocaleString(language, {
+      useGrouping: true,
+      maximumFractionDigits: 6,
+    });
+
+    // Add back missing .0 in e.g. 12.0
+    const match = value.match(/\.\d*?(0*)$/);
+
+    if (match) formattedValue += /[1-9]/.test(match[0]) ? match[1] : match[0];
+
+    return (
+      <div {...props} className="calculator-display">
+        <AutoScalingText>{formattedValue}</AutoScalingText>
+      </div>
+    );
+  }
+}
+
+class CalculatorKey extends React.Component {
+  render() {
+    const { className, ...props } = this.props;
+
+    return <button className={`calculator-key ${className}`} {...props} />;
+  }
+}
+
+class Calculator extends React.Component {
+  state = {
+    value: null,
+    displayValue: '0',
+    operator: null,
+    waitingForOperand: false,
+  };
+
+  render() {
+    const { displayValue } = this.state;
+
+    const clearDisplay = displayValue !== '0';
+    const clearText = clearDisplay ? 'C' : 'AC';
+
+    return (
+      <div className="calculator">
+        <CalculatorDisplay value={displayValue} />
+        <div className="calculator-keypad">
+          <div className="input-keys">
+            <div className="function-keys">
+              <CalculatorKey
+                className="key-clear"
+                onPress={() =>
+                  clearDisplay ? this.clearDisplay() : this.clearAll()
+                }
+              >
+                {clearText}
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-sign"
+                onPress={() => this.toggleSign()}
+              >
+                ±
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-percent"
+                onPress={() => this.inputPercent()}
+              >
+                %
+              </CalculatorKey>
+            </div>
+            <div className="digit-keys">
+              <CalculatorKey
+                className="key-0"
+                onPress={() => this.inputDigit(0)}
+              >
+                0
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-dot"
+                onPress={() => this.inputDot()}
+              >
+                ●
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-1"
+                onPress={() => this.inputDigit(1)}
+              >
+                1
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-2"
+                onPress={() => this.inputDigit(2)}
+              >
+                2
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-3"
+                onPress={() => this.inputDigit(3)}
+              >
+                3
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-4"
+                onPress={() => this.inputDigit(4)}
+              >
+                4
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-5"
+                onPress={() => this.inputDigit(5)}
+              >
+                5
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-6"
+                onPress={() => this.inputDigit(6)}
+              >
+                6
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-7"
+                onPress={() => this.inputDigit(7)}
+              >
+                7
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-8"
+                onPress={() => this.inputDigit(8)}
+              >
+                8
+              </CalculatorKey>
+              <CalculatorKey
+                className="key-9"
+                onPress={() => this.inputDigit(9)}
+              >
+                9
+              </CalculatorKey>
+            </div>
+          </div>
+          <div className="operator-keys">
+            <CalculatorKey
+              className="key-divide"
+              onPress={() => this.performOperation('/')}
+            >
+              ÷
+            </CalculatorKey>
+            <CalculatorKey
+              className="key-multiply"
+              onPress={() => this.performOperation('*')}
+            >
+              ×
+            </CalculatorKey>
+            <CalculatorKey
+              className="key-subtract"
+              onPress={() => this.performOperation('-')}
+            >
+              −
+            </CalculatorKey>
+            <CalculatorKey
+              className="key-add"
+              onPress={() => this.performOperation('+')}
+            >
+              +
+            </CalculatorKey>
+            <CalculatorKey
+              className="key-equals"
+              onPress={() => this.performOperation('=')}
+            >
+              =
+            </CalculatorKey>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
