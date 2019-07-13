2a5943a1f67e2369d7396a71920bf3f503e4bd0c
jcunanan05 <j_cunanan05@yahoo.com>
2019-07-13 19:22:28 -0400

    Add auto scaling text

--- src/Calculator/AutoScalingText.js
+++ src/Calculator/AutoScalingText.js
@@ -5,10 +5,32 @@ class AutoScalingText extends React.Component {
     scale: 1,
   };
 
+  componentDidUpdate = () => {
+    const { state } = this;
+    const node = this.node;
+    const availableWidth = node.parentNode.offsetWidth;
+    const actualWidth = node.offsetWidth;
+    const actualScale = availableWidth / actualWidth;
+    if (state.scale === actualScale) return;
+    if (actualScale < 1) {
+      this.setState({ scale: actualScale });
+    } else if (state.scale < 1) {
+      this.setState({ scale: 1 });
+    }
+  };
+
   render() {
     const { scale } = this.state;
 
-    return <div className="auto-scaling-text">{this.props.children}</div>;
+    return (
+      <div
+        className="auto-scaling-text"
+        style={{ transform: `scale(${scale},${scale})` }}
+        ref={node => (this.node = node)}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
 
